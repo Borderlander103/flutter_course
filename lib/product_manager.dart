f94965aa643dccf236c172e04b691ff03816793f
Borderlander103 <mailbox@brianrichardsononline.com>
2018-10-08 21:10:36 -0700

    Add debug print statements and didUpdateWidget

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -5,10 +5,13 @@ import './products.dart';
 class ProductManager extends StatefulWidget {
 final String startingProduct;
 
-  ProductManager(this.startingProduct);
+  ProductManager(this.startingProduct) {
+    print('[ProductManager Widget] Constructor');
+  }
 
   @override
   State<StatefulWidget> createState() {
+    print('[ProductManager Widget] createState()');
     return _ProductManagerState();
   }
 }
@@ -19,11 +22,19 @@ class _ProductManagerState extends State<ProductManager> {
   @override
   void initState() {
     super.initState();
+    print('[ProductManager State] initState()');
     _products.add(widget.startingProduct);
   }
 
   @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
     return Column(children: [Container(
       margin: EdgeInsets.all(10.0),
       child: RaisedButton(
