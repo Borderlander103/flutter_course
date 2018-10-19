426fcedc14b98ef8fc2335e3fde7b99252be5924
Borderlander103 <mailbox@brianrichardsononline.com>
2018-10-19 06:44:19 -0700

    Add ButtonBar to product and add product page

--- lib/products.dart
+++ lib/products.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 
-class Products extends StatelessWidget{
+class Products extends StatelessWidget {
   final List<String> products;
 
   Products(this.products) {
     print('[Products Widget] Constructor');
   }
 
-Widget  _buildProductItem(BuildContext context, int index) {
-  return Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/food.jpg'),
-                      Text(products[index])
-                    ],
-                  ),
-                );
+  Widget _buildProductItem(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset('assets/food.jpg'),
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => {},
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 
   Widget _buildProductList() {
@@ -28,14 +37,14 @@ Widget  _buildProductItem(BuildContext context, int index) {
     } else {
       productCard = Center(
         child: Text('No Products found, please add some.'),
-        );
+      );
     }
     return productCard;
   }
 
   @override
-    Widget build(BuildContext context) {
-      print('[Products Widget] build()');
-      return _buildProductList();
-    }
+  Widget build(BuildContext context) {
+    print('[Products Widget] build()');
+    return _buildProductList();
+  }
 }
