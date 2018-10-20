a4dd2e413fde6ad9e692b945dd2493dddccefab0
Borderlander103 <mailbox@brianrichardsononline.com>
2018-10-20 09:12:07 -0700

    Format Details page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -8,11 +8,20 @@ class ProductPage extends StatelessWidget {
         title: Text('Product Detail'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Text('Details!'),
-          RaisedButton(
-            child: Text('BACK'),
-            onPressed: () => Navigator.pop(context),
+          Image.asset('assets/food.jpg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              color: Theme.of(context).primaryColorLight,
+              child: Text('BACK'),
+              onPressed: () => Navigator.pop(context),
+            ),
           )
         ],
       ),
